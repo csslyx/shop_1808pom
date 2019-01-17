60c6d59d71c1736cb440f23af957d2f886350765
csslyx <46553993+csslyx@users.noreply.github.com>
2019-01-17 17:10:40 +0800

    Revert "首页工程、搜索工程、搜索服务的搭建"

--- shop_entity/src/main/java/com/qf/entity/Goods.java
+++ shop_entity/src/main/java/com/qf/entity/Goods.java
@@ -1,10 +1,6 @@
 package com.qf.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -14,11 +10,8 @@ import java.io.Serializable;
  * @Version 1.0
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Goods implements Serializable {
 
-    @TableId(type = IdType.AUTO)
     private int id;
     private String title;
     private String ginfo;
